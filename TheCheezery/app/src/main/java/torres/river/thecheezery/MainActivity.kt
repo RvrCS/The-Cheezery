ebfa1c19904375c46f77b36a1fd9eeeea31eec27
River <75063620+RvrCS@users.noreply.github.com>
2023-02-09 01:20:04 -0700

    Finished
    Change between activites using buttons

--- TheCheezery/app/src/main/java/torres/river/thecheezery/MainActivity.kt
+++ TheCheezery/app/src/main/java/torres/river/thecheezery/MainActivity.kt
@@ -1,11 +1,24 @@
 package torres.river.thecheezery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: AppCompatButton = findViewById(R.id.button_get_started)
+
+        button.setOnClickListener {
+            var intent: Intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+
+
+        }
+
+
     }
 }
--- TheCheezery/app/src/main/java/torres/river/thecheezery/RegisterActivity.kt
+++ TheCheezery/app/src/main/java/torres/river/thecheezery/RegisterActivity.kt
@@ -1,11 +1,22 @@
 package torres.river.thecheezery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val button: AppCompatButton = findViewById(R.id.button_sign_in)
+
+        button.setOnClickListener {
+            var intent: Intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+            
+
+        }
     }
 }
